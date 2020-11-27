4b1a655084aad9d27ff0bc4f2659eca76399ad6d
Vera Kutsenko <verakutsenko@Veras-MacBook-Pro.local>
2020-11-26 17:07:28 -0700

    amend prod url

--- client/src/lib/config.js
+++ client/src/lib/config.js
@@ -11,7 +11,7 @@ export default {
   dateFormat: 'Do MMM YYYY',
 
   // API
-  apiBaseUrl: isDevEnv ? 'http://localhost:5000' : 'https:/www.enterneverland.com',
+  apiBaseUrl: isDevEnv ? 'http://localhost:5000' : 'https://www.enterneverland.com',
   // apiBaseUrl: isDevEnv ? 'http://192.168.4.76:5000' : 'https://www.enterneverland.com',
 
   // Google Analytics - uses a 'dev' account while we're testing
