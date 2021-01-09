98a737e8d9546107947d578f5064e0c3982776dc
Vera Kutsenko <verakutsenko@Veras-MacBook-Pro.local>
2021-01-08 19:06:46 -0700

    remove storeId & vendorId

--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -650,8 +650,8 @@ router.post('/seller/update', async function(req, res, next) {
   let title = formData.title;
   let originZipCode = formData.originZipCode;
   let handlingFee = formData.handlingFee;
-  let storeId = formData.storeId;
-  let vendorId = formData.userId;
+  //let storeId = formData.storeId;
+  //let vendorId = formData.userId;
   let isArtificial = formData.isArtificial ? formData.isArtificial : false;
   let isOrganic = formData.isOrganic ? formData.isOrganic : false;
   let isVisible = formData.isVisible ? formData.isVisible: false;
